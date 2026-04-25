147f1db25e90c03a3809776010501255672169e6
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-24 21:09:25 -0600

    [ADD]: Formulario dinamico en base el endpoint donde estamos con bg de Tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 // * rfc para cargar componente basico
 // import { Link, NavLink } from "react-router-dom"
-import { NavLink } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 // import { Link } from "react-router-dom"
 
+
+
 export default function Header() {
+  const {pathname} = useLocation()
+
+  const isHome = useMemo(() => pathname === '/', [pathname])
+  console.log(isHome);
+  
+  
   return (
-    <header className="bg-slate-800">
-      <div className="mx-auto container px-4 py-16">
+    <header className={ isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
+      <div className="mx-auto container px-4 py-10">
         <div className="flex justify-between items-center">
           <div>
             <img src="../public/logo.svg" alt="Logotipo" className="w-32"/>
@@ -31,6 +40,51 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+
+        { isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-10 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label 
+                htmlFor="ingredient"
+                className="text-white uppercase font-extrabold text-lg"
+                >
+                  Nombre o Ingredientes
+                </label>
+
+                <input
+                  id="ingredient"
+                  type="text"
+                  name="ingredient" // * Para colocarlo en el STATE
+                  className="p-3 w-full rounded-lg focus:outline-none"
+                  placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe, etc."
+                />
+            </div>
+
+            <div className="space-y-4">
+              <label 
+                htmlFor="ingredient"
+                className="text-white uppercase font-extrabold text-lg"
+                >
+                  Categoria
+                </label>
+
+                <select
+                  id="ingredient"
+                  name="ingredient" // * Para colocarlo en el STATE
+                  className="p-3 w-full rounded-lg focus:outline-none"
+                >
+                  <option value="">-- Seleccione una categoría--</option>
+                </select>
+            </div>
+
+            <input 
+              type="submit"
+              value='Buscar Recetas'
+              className="cursor-pointer bg-orange-700 hover:bg-orange-800 text-white font-extrabold w-full p-2 uppercase rounded-lg transition-all duration-300 ease-in"
+            />
+          </form>
+        )}
+
       </div>
     </header>
   )
