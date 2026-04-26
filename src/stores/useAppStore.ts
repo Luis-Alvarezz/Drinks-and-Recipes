f433a1f9f66fda6c39a6905b1c4cf2913bf98831
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-26 14:13:31 -0600

    [MODIFY]: Se añadio Redux Dev Tools

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,8 @@
 // * STORE PRINCPAL - Recibe los SLICES para Unificar el Store
 import { create } from "zustand";
 import { createRecipiesSlice, type RecipiesSliceType } from "./recipeSlice";
+import { devtools } from "zustand/middleware";
 
-export const useAppStore = create<RecipiesSliceType>( (...a) => ({
+export const useAppStore = create<RecipiesSliceType>()(devtools((...a) => ({
   ...createRecipiesSlice(...a) // * Llamado con COPIA del recipeSlice y pasamos argumento o metodos de set, get, etc. 
-}))
+})))
