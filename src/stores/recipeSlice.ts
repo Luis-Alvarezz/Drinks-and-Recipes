785f7d61546415f083231b2db8df47f422310991
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-26 12:47:11 -0600

    [NODIFY]: Se agrego primera ACCION para cargar resp de la API mediante primer SLICE

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,18 @@
 import type { StateCreator } from "zustand"
+import { getCategories } from "../services/RecipeService"
 
 type Category = []
 
 export type RecipiesSliceType = {
-  categories: Category[]
+  categories: Category[],
+  fetchCategories: () => Promise<void>
 }
 
 export const createRecipiesSlice : StateCreator<RecipiesSliceType> = () => ({
-  categories: [] // * STATE
+  categories: [], // * STATE
+
+  // * Acciones (DISPATCH)
+  fetchCategories: async() => {
+    getCategories()
+  }
 })
