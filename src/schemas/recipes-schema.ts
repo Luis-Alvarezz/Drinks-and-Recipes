39a9ec9a007034a45991225450067bb0c2e59691
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-30 21:49:32 -0600

    [MODIFY]: Se cambio el Esquema porque NO eres resp de API

--- src/schemas/recipes-schema.ts
+++ src/schemas/recipes-schema.ts
@@ -7,8 +7,9 @@ export const CategoriesAPIResponseSchema = z.object({
   }))
 })
 
-// ! 1.2 Creando el ESQUEMA para la API al obtener las Recetas
-export const SearchFilterAPIResponseSchema = z.object({
+// ! 1.2 Creando el ESQUEMA para el envio de Form por parte del usuario al obtener las Recetas
+export const SearchFilterResponseSchema = z.object({
   ingredient: z.string(),
   category: z.string()
 })
+
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 import z from "zod"
-import { CategoriesAPIResponseSchema, SearchFilterAPIResponseSchema } from "../schemas/recipes-schema"
+import { CategoriesAPIResponseSchema, SearchFilterResponseSchema } from "../schemas/recipes-schema"
 
 // ! 2. Creando el INFERIR TYPE para la API al obtener las categorias
 export type CategoriesInfer = z.infer<typeof CategoriesAPIResponseSchema> // * Antes: Category[] -> Pero el arreglo viene dentro de la resp de la API
 
 // ! 2.1 Creando el INFER TYPE para la API al obtener las recetas
-export type SearchFilterInfer = z.infer<typeof SearchFilterAPIResponseSchema>
+export type SearchFilterInfer = z.infer<typeof SearchFilterResponseSchema>
