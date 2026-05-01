96b52ae5ef5a426dd20001e5afaee56a512c9e08
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-01 16:22:09 -0600

    [ADD]: Cargar res API, mostrar componente y CSS para Bebidas

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,33 @@
 // * RFC - Para cargar plantilla inicial
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+import DrinkCard from "../components/DrinkCard"
 
 export default function IndexPage() {
+  const drinks = useAppStore((state) => state.drinksResAPI)
+
+  const hasDrinks = useMemo(() => drinks.drinks.length > 0, [drinks])
+
   return (
     <>
-      <h1>Inicio</h1>
+      {
+        hasDrinks ? (
+          <>
+            <h1 className="text-6xl font-extrabold">Recetas</h1>
+            {
+              drinks.drinks.map(drink => {
+                return <DrinkCard
+                          key={drink.idDrink}
+                          drink={drink}
+                        />
+              })
+            }
+          </>
+        ) : 
+        (
+          <h1 className="my-10 text-center text-2xl text-red-600 font-bold">No hay resultados aún, llena el formulario para buscar recetas</h1> 
+        )
+      }
     </>
   )
 }
