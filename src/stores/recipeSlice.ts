0e01494c0678e87fda737100ce65be5a16098fa2
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-30 22:21:41 -0600

    [ADD]: Accion 2 para obtener bebidas del service y colocar en STATE

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,16 +1,22 @@
 import type { StateCreator } from "zustand"
-import { getCategories } from "../services/RecipeService"
-import type { Categories } from "../types/types"
+import { getCategories, getRecipies } from "../services/RecipeService"
+import type { CategoriesInfer, DrinksInfer, SearchFilterInfer } from "../types/types"
 
 
 export type RecipiesSliceType = {
-  categories: Categories,
+  categories: CategoriesInfer,
+  drinksResAPI: DrinksInfer,
   fetchCategories: () => Promise<void>
+  fetchSearchRecipies: (searchFilters: SearchFilterInfer) => Promise<void>
 }
 
 export const createRecipiesSlice : StateCreator<RecipiesSliceType> = (set) => ({
   // categories: [], // * STATE
   categories: {
+    drinks: []
+  },
+
+  drinksResAPI: {
     drinks: []
   },
 
@@ -20,6 +26,17 @@ export const createRecipiesSlice : StateCreator<RecipiesSliceType> = (set) => ({
     // console.log('Respuesta de API desde recipeSlice: ',APIResponseCategories);
     set(() => ({
       categories: APIResponseCategories
+    }))
+  },
+
+  // * Accion 2 - Consultar las Recetas por medio la API
+  fetchSearchRecipies: async(searchFilters) => {
+    // console.log('Desde Accion 2 searchRecipies, recipeSlice');
+    // console.log(searchFilters); // * Ya se recibe ingredient y category de Form
+    const drinks = await getRecipies(searchFilters)
+    // console.log(drinks); // * {drinks: Array(100)}
+    set(() => ({
+      drinksResAPI: drinks
     }))
   }
 })
