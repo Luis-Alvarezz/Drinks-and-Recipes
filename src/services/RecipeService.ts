17e12a40c21a398b1a6bcdc733c25b2c019d30c8
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-02 18:08:41 -0600

    [ADD]: endpoint para API de detalles bebida

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { CategoriesAPIResponseSchema, DrinksAPIResponseSchema } from "../schemas/recipes-schema";
-import type { SearchFilterInfer } from "../types/types";
+import { CategoriesAPIResponseSchema, DrinksAPIResponseSchema, RecipeAPIResponseSchema } from "../schemas/recipes-schema";
+import type { DrinkInfer, SearchFilterInfer } from "../types/types";
 
 export async function getCategories() {
   // console.log('Desde Servicio RecipeServices');
@@ -27,6 +27,19 @@ export async function getRecipies(filters: SearchFilterInfer) {
   const result = DrinksAPIResponseSchema.safeParse(data)
   // console.log(result); // * {success: true, data: {…}} ->  drinks : (100) [{…}, {…}, ... , {…}] 
   
+  if (result.success) {
+    return result.data
+  }
+}
+
+// ! Metodo 3.- Obtener los detalles de la bebida seleccionada mediante ID
+export async function getDetailsRecipieByID(drinkID: DrinkInfer['idDrink']) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`
+  // console.log(url);
+  const { data } = await axios(url)
+  // console.log(data);
+  const result = RecipeAPIResponseSchema.safeParse(data.drinks[0]) // * Al leer el log anterior, observamos como es un objeto dentro de un arreglo[0]
+  // console.log(result);
   if (result.success) {
     return result.data
   }
