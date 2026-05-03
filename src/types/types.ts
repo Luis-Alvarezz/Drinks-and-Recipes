ec0030bf176c4db1f2c27fd047cfe1ce4ffc1614
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-02 18:15:01 -0600

    [ADD]: INFER TYPE para ESQUEMA res API detalles Bebida

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 import z from "zod"
-import { CategoriesAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterResponseSchema } from "../schemas/recipes-schema"
+import { CategoriesAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, RecipeAPIResponseSchema, SearchFilterResponseSchema } from "../schemas/recipes-schema"
 
 // ! 2. Creando el INFERIR TYPE para la API al obtener las categorias
 export type CategoriesInfer = z.infer<typeof CategoriesAPIResponseSchema> // * Antes: Category[] -> Pero el arreglo viene dentro de la resp de la API
@@ -12,3 +12,6 @@ export type DrinksInfer = z.infer<typeof DrinksAPIResponseSchema>
 
 // ! 4.1 Creando INFER TYPE para componente DrinkCard y leer bebida por bebida
 export type DrinkInfer = z.infer<typeof DrinkAPIResponseSchema>
+
+// ! 5.1. Creando INFER TYPE para resp de API al obtener detalles de bebida por ID
+export type RecipeInfer = z.infer<typeof RecipeAPIResponseSchema>
