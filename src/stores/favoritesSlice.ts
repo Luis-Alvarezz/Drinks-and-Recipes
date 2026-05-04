6f6a687688cf0a5cd5f00be48590a501cdb317f3
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-04 17:32:51 -0600

    [ADD]: Accion para settear la bebida fav en STATE

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -3,8 +3,32 @@ import type { RecipeInfer } from "../types/types"
 
 export type FavoritesSliceType = {
   favorites: RecipeInfer[] // * Array porque pueden ser 2 o mas favoritos
+  handleClickFavorite: (recipe: RecipeInfer) => void
 }
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
-  favorites: []
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+  // * STATES
+  favorites: [],
+
+  // * Acciones o Dispatch
+  handleClickFavorite: (recipe) => {
+    // console.log('Desde favoriteSlice, metodo handleClickFavorite',recipe);
+    // console.log(get().favorites);
+    if (get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
+      // console.log('Si existe');
+      set((state) => ({
+        favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+      }))
+    } else {
+      // console.log('No existe');
+      // * Al profe no le gusta, pero funciona:
+      // set({
+      //   favorites: [...get().favorites, recipe]
+      // })
+      // * Forma Limpia:
+      set((state) => ({
+        favorites: [...state.favorites, recipe]
+      }))
+    }
+  }
 })
