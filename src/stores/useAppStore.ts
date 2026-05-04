36d9ecda153c0d5bfcd813b557c9c81d293c2b61
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-03 22:40:53 -0600

    [MODIFY]: Se agrego SLICE y Type para Favoritos

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,7 +2,9 @@
 import { create } from "zustand";
 import { createRecipiesSlice, type RecipiesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 
-export const useAppStore = create<RecipiesSliceType>()(devtools((...a) => ({
-  ...createRecipiesSlice(...a) // * Llamado con COPIA del recipeSlice y pasamos argumento o metodos de set, get, etc. 
+export const useAppStore = create<RecipiesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+  ...createRecipiesSlice(...a), // * Llamado con COPIA del recipeSlice y pasamos argumento o metodos de set, get, etc. 
+  ...createFavoritesSlice(...a) // * Llamado con COPIA del favoritesSlice y pasamos argumento o metodos de set, get, etc. 
 })))
