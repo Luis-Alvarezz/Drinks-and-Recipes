6358d549dc92d49881b5dec1f0184d4211aa5192
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-04 21:48:05 -0600

    [ADD]: Llamando al SliceStore de la Notificacion

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,8 +3,10 @@ import { create } from "zustand";
 import { createRecipiesSlice, type RecipiesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
+import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
 
-export const useAppStore = create<RecipiesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipiesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
   ...createRecipiesSlice(...a), // * Llamado con COPIA del recipeSlice y pasamos argumento o metodos de set, get, etc. 
-  ...createFavoritesSlice(...a) // * Llamado con COPIA del favoritesSlice y pasamos argumento o metodos de set, get, etc. 
+  ...createFavoritesSlice(...a), // * Llamado con COPIA del favoritesSlice y pasamos argumento o metodos de set, get, etc. 
+  ...createNotificationSlice(...a) // * Llamado con COPIA del notificationSlice y pasamos argumento o metodos de set, get, etc. 
 })))
