ab494757bf0f1761fa177beec3b6fd6e9db1558b
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-05 22:11:26 -0600

    [ADD]: Array de Dependencia a useEffect y Componente Notification

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,13 +4,15 @@ import Header from "../components/Header";
 import Modal from "../components/Modal";
 import { useEffect } from "react";
 import { useAppStore } from "../stores/useAppStore";
+import Notification from "../components/Notification";
 
 export default function Layout() {
   const loadFromStorage = useAppStore((state) => state.loadFromStorage)
   
   useEffect(() => {
     loadFromStorage()
-  }, [])
+  }, [loadFromStorage])
+  
   return (
     <>
       {/* <div>Layout</div> */}
@@ -21,6 +23,7 @@ export default function Layout() {
       </main>
 
       <Modal />
+      <Notification />
     </>
   )
 }
