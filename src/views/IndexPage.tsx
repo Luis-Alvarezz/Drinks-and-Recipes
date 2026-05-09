0a6996e743cea7a665e080fa8997376984113d56
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-09 15:13:53 -0600

    [MODIFY]: Cambio color de titulo

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -10,7 +10,7 @@ export default function IndexPage() {
 
   return (
     <>
-      <h1 className="text-6xl font-extrabold text-center text-green-700">Recetas</h1>
+      <h1 className="text-6xl font-extrabold text-center text-black">Recetas</h1>
       {
         hasDrinks ? (
           <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
