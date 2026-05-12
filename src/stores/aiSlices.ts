321cfc9ee85380e2aecb6705ffca8eb2f639e049
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-11 22:49:58 -0600

    [ADD]: Metodo para escribir en el STATE en base la res del Modelo

--- src/stores/aiSlices.ts
+++ src/stores/aiSlices.ts
@@ -1,13 +1,21 @@
 import type { StateCreator } from "zustand"
+import { generateRecipe } from "../services/AiService"
 
 export type AiSliceType = {
   recipe: string
   generateRecipe: (prompt: string) => Promise<void>
 }
-export const createAISlice: StateCreator<AiSliceType, [], [], AiSliceType> = () => ({
+export const createAISlice: StateCreator<AiSliceType, [], [], AiSliceType> = (set) => ({
   recipe: '',
 
   generateRecipe: async (prompt) => {
-    console.log('Desde genereateRecipe', prompt);
+    // console.log('Desde genereateRecipe', prompt);
+    const data  = await generateRecipe(prompt)
+    for await (const textPart of data) {
+      // console.log(textPart);
+      set((state) =>  ({
+        recipe: state.recipe + textPart // * Copia de recipe + respu sig
+      }))
+    }
   }
 })
